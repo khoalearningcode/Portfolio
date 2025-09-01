66b72901f575dd664f90f2efd3586de0d1e5bda8
Khoalearningcode <godminhkhoa@gmail.com>
2025-09-01 16:52:01 +0700

    f

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,10 +2,10 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { ThemeSwitcher } from './theme-switcher';
 import { cn } from '@/lib/utils';
 import { Menu, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { ThemeProvider } from '../theme-provider';
 
 const navLinks = [
   { href: '#about', label: 'About' },
@@ -46,7 +46,7 @@ const Header = () => {
               {link.label}
             </Link>
           ))}
-          <ThemeSwitcher />
+          <ThemeProvider />
         </nav>
         <div className="md:hidden">
           <Button variant="ghost" size="icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
@@ -68,7 +68,7 @@ const Header = () => {
                 {link.label}
               </Link>
             ))}
-            <ThemeSwitcher />
+            <ThemeProvider />
           </nav>
         </div>
       )}
